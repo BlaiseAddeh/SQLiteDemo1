cb468d696c81db9afe3ccb7455f514a6007db781
BLAISEPC <addehblaise@yahoo.fr>
2020-11-23 17:11:03 +0000

    Commit2

--- app/src/main/java/com/example/sqlite/MainActivity.java
+++ app/src/main/java/com/example/sqlite/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.sqlite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FloatingActionButton addRecordBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addRecordBtn = findViewById(R.id.addRecordBtn);
+
+        addRecordBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                  startActivity(new Intent(MainActivity.this, AddUpdateRecordActivity.class));
+
+            }
+        });
+
+
     }
 }
